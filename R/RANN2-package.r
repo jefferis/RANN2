ada2c9027be755e52bfb53935d65048ee7cc5147
Gregory Jefferis <jefferis@gmail.com>
2014-08-06 20:12:48 +0100

    package docs + setup roxygen2

---
+++ R/RANN2-package.r
@@ -0,0 +1,8 @@
+#' Wrapper for Arya and Mount's Approximate Nearest Neighbours (ANN) C++ library
+#' 
+#' @name RANN2-package
+#' @aliases RANN2
+#' @seealso \code{\link{nn2}}
+#' @useDynLib RANN2
+#' @importFrom Rcpp evalCpp
+NULL
