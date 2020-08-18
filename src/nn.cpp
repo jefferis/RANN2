71511fcc48e2db29c981f677d3fb58fa292e7521
Gregory Jefferis <jefferis@gmail.com>
2020-08-18 13:17:50 +0100

    Simplify nn2 since queryFR handles both search types

--- src/nn.cpp
+++ src/nn.cpp
@@ -9,10 +9,6 @@ using namespace Rcpp;
 List nn2_cpp(NumericMatrix data, NumericMatrix query, const int k, const double eps = 0.0, const double radius = NA_REAL) {
 
   WANN tree = WANN(data);
-
-  if(R_IsNA(radius)) {
-    return tree.query(query, k, eps);
-  } else {
-    return tree.query_FR(query, k, radius, eps);
-  }
+  // NB this method will do a regular search when radius=0 or NA
+  return tree.query_FR(query, k, radius, eps);
 }
